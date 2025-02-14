205b0c454748b733c776069884fee985eefafc59
viviana paredes <vivianap4redes@gmail.com>
2025-02-14 11:14:19 -0500

    Creamos una aplicacion de carrito de compras para el proyecto final de REACT

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { CarritoApp } from './CarritoApp'
+import { BrowserRouter } from 'react-router-dom'
+
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <StrictMode>  
+    <CarritoApp/> 
+  </StrictMode>,
+  </BrowserRouter>
+)
